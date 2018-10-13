552011d2b7d3983f31cb2affa8ea45344704a2f2
Linda <clinda.se@gmail.com>
2018-10-13 12:52:29 -0700

    Prototype working

--- Fizzbuzz.rb
+++ Fizzbuzz.rb
@@ -2,6 +2,16 @@
 number = 1
 
 while number <= 100
-  puts number
+
+  if number % 3 == 0 && number % 5 == 0
+    puts "FizzBuzz"
+  elsif number % 3 == 0
+    puts "Fizz"
+  elsif number % 5 == 0
+    puts "Buzz"
+  else
+    puts number
+  end
+
   number += 1
 end
